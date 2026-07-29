f54a45ed9581b1e8962aa4781680889a173522be
31dylanj-pixel <31dylan_j@students.ics.ac.th>
2026-07-29 13:34:50 +0700

    Update notifications with new messages

---
+++ notifications.js
@@ -0,0 +1,29 @@
+const notifications = [
+    "Next Meeting: August 15",
+    "Sign Ups Opening Soon",
+    "Website Under Development"
+];
+
+let notificationIndex = 0;
+
+const notificationText = document.getElementById("notification-text");
+
+function updateNotification() {
+    notificationText.style.opacity = 0;
+
+    setTimeout(() => {
+        notificationText.textContent = notifications[notificationIndex];
+        notificationText.style.opacity = 1;
+
+        notificationIndex++;
+
+        if (notificationIndex >= notifications.length) {
+            notificationIndex = 0;
+        }
+
+    }, 300);
+}
+
+updateNotification();
+
+setInterval(updateNotification, 5000);
