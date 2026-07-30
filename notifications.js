86006f1498a80ad5a546272aa7172416fcb5864c
31dylanj-pixel <31dylan_j@students.ics.ac.th>
2026-07-30 21:19:06 +0700

    Add new notification for D Camp dates

--- notifications.js
+++ notifications.js
@@ -4,6 +4,7 @@ const notifications = [
     "Website Under Development",
     "Join the Club!",
     "On July 18th, 11 year old Bodhana Sivanandan scored her first IM Norm, breaking a 38 Year Old Record"
+    "D Camp: July 31 - August 2"
 ];
 
 let notificationIndex = 0;
